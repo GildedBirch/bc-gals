ce88a6d6fe0fbdff73dcf796455c591c32914511
GildedBirch <thebirchbreaker@gmail.com>
2026-01-31 17:43:47 +0200

    LevelData resource to hold information about leves

---
+++ _resources/scripts/level_data.gd
@@ -0,0 +1,13 @@
+class_name LevelData
+extends Resource
+
+
+## 0: Empty space[br]
+## 1: Breakable bricks[br]
+## 2: Indestructible wall[br]
+## 3: Defense objective[br]
+## 4: Player spawn[br]
+## 5: Enemy spawn
+@export var map_layout: Array[Array]
+@export var id: int
+@export var name: String
