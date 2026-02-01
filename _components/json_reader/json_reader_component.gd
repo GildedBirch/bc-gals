1736982478eb14af649c35cbf4e7b9034caee0c6
GildedBirch <thebirchbreaker@gmail.com>
2026-02-01 14:14:24 +0200

    JSONReader can now read from specified folder

--- _components/json_reader/json_reader_component.gd
+++ _components/json_reader/json_reader_component.gd
@@ -2,6 +2,26 @@ class_name JSONReaderComponent
 extends Node
 
 
+@export var folder: String
+
+
+func read_from_folder(file_name: String) -> Dictionary:
+	if file_name == "":
+		assert(false, "File name not given.")
+	if folder == "":
+		assert(false, "No folder set.")
+	
+	var file: FileAccess = FileAccess.open("%s%s" % [folder, file_name], FileAccess.READ)
+	var json_string: String = file.get_as_text()
+
+	var json: JSON = JSON.new()
+	var parse_result: Error = json.parse(json_string)
+	if not parse_result == OK:
+		assert(false, "JSON Parse Error: %s in %s at line %s" % [json.get_error_message(), json_string, json.get_error_line()])
+	
+	return json.data
+
+
 func read(file_path: String) -> Dictionary:
 	if file_path == "":
 		assert(false, "File path not given.")
