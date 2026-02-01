2af6cf87619b92f384274a8e18dc235edddd4854
GildedBirch <thebirchbreaker@gmail.com>
2026-02-01 14:50:54 +0200

    GameWorld now loads first level on startup

--- _core/game_world/game_world.gd
+++ _core/game_world/game_world.gd
@@ -1,2 +1,13 @@
 class_name GameWorld
 extends Node
+
+
+@onready var map: Map = %Map
+@onready var players: Node3D = %Players
+@onready var enemies: Node3D = %Enemies
+@onready var bullets: Node3D = %Bullets
+@onready var powerups: Node3D = %Powerups
+
+
+func _ready() -> void:
+	map.load_level(0)
